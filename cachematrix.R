b54db9eb51343eb10d016fcda157909f9c4e6777
NagarAlex <70220811+NagarAlex@users.noreply.github.com>
2020-08-25 18:04:45 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Matrix form of makeVector from example
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {m <- NULL
+set <- function(y){
+  x <<- y
+  m <<- NULL
+}
+get <- function(){x}
+setinverse<- function(solve){m <<- solve}
+getinverse <- function(){m}
+list(set = set, get = get, setinverse = setinverse, getinverse
+     = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
+## Parallel form of cachemean from the example but with matrix inverse instead of vector mean
 
 cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)){message("getting cached data")
+    return(m)}
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  
         ## Return a matrix that is the inverse of 'x'
+  m
 }
